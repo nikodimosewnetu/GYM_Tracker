5532b10b1a0bd3410ea9024370eba6f6afe40ba3
Nikodimos-Developer <98909873+nikodimosewnetu@users.noreply.github.com>
2025-02-01 22:43:27 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Login from "./pages/Login";  // Import Login Page
+import Login from "./pages/Login"; // Import Login Page
 import Workouts from "./pages/Workouts";
 import { AuthProvider } from "./context/AuthContext"; // Add this import
 
@@ -9,15 +9,12 @@ function App() {
   return (
     <AuthProvider> {/* Wrap your entire app with AuthProvider */}
       <BrowserRouter>
-      <Router>
-       
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} /> {/* Add this line */}
           <Route path="/workouts" element={<Workouts />} />
         </Routes>
-      </Router>
       </BrowserRouter>
     </AuthProvider>
   );
