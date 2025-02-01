7cd950db2f129c5d8bf84caa4411f9258ccd6151
nikodimosewnetu <nikodimosewnetu@gmail.com>
2025-02-01 23:06:16 +0300

    Updated build configuration to use Vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Correctly import BrowserRouter
 import Home from "./pages/Home";
 import Register from "./pages/Register";
-import Login from "./pages/Login"; // Import Login Page
+import Login from "./pages/Login";
 import Workouts from "./pages/Workouts";
-import { AuthProvider } from "./context/AuthContext"; // Add this import
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider> {/* Wrap your entire app with AuthProvider */}
-      <BrowserRouter>
+    <AuthProvider>  {/* Wrap your entire app with AuthProvider */}
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} /> {/* Add this line */}
+          <Route path="/login" element={<Login />} />
           <Route path="/workouts" element={<Workouts />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </AuthProvider>
   );
 }
