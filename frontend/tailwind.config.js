d2f59208269c9df6dc2ac1181177afcf371e2dc2
Nikodimos-Developer <98909873+nikodimosewnetu@users.noreply.github.com>
2025-02-01 18:58:36 +0300

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}',  // Adjust paths to your HTML/JSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
