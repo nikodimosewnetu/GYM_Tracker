fb33ec167caceea7d089cbb7b9b6c8363ef5d4d5
nikodimosewnetu <nikodimosewnetu@gmail.com>
2025-02-02 10:44:33 +0300

    Your commit message

--- frontend/src/api/axiosInstance.jsx
+++ frontend/src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Fetch the token each time an API request is made
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://gym-tracker-5.onrender.com/api/',
 });
 
 axiosInstance.interceptors.request.use((config) => {
